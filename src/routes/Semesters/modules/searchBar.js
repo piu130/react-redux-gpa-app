6b17c410f49f7baffc2a81a68afc6fd4ac822808
Ramon Boss <ramon.boss@outlook.com>
2017-03-06 19:55:23 +0100

    removed unused import

--- src/routes/Semesters/modules/searchBar.js
+++ src/routes/Semesters/modules/searchBar.js
@@ -1,4 +1,4 @@
-import { FILTER_SEMESTERS, filterSemesters } from './semestersMainView'
+import { filterSemesters } from './semestersMainView'
 
 export const searchValueChanged = (event, newValue, previousValue) => (dispatch) => {
   dispatch(filterSemesters(newValue))
