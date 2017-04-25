8fdef0a556b2b0d766ee12b4f8e5becbfde547ef
Ramon Boss <ramon.bossooutlook.com>
2017-04-25 10:19:32 +0200

    updated redux-logger

--- src/store/middleware/index.js
+++ src/store/middleware/index.js
@@ -1,9 +1,9 @@
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
+import logger from 'redux-logger'
 import nProgressMiddleware from './nprogress-middleware'
 
 export default [
   thunk,
-  createLogger(),
+  logger,
   nProgressMiddleware
 ]
