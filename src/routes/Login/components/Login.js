45e5710f16517c098678b1cacf5b18098c6127b1
Ramon Boss <ramon.bossooutlook.com>
2017-04-25 10:02:20 +0200

    link to register

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -1,29 +1,33 @@
 import React from 'react'
+import { browserHistory } from 'react-router'
 import { Form, Field, reduxForm } from 'redux-form'
 import { FormGroup, ControlLabel, Button } from 'react-bootstrap'
 import { loginSucceed, loginFailed } from '../modules/login'
 
 export const Login = (props) => (
-  <Form onSubmit={props.handleSubmit(props.login)} form='form'>
-    <FormGroup>
-      <ControlLabel>Email</ControlLabel>
-      <Field name='email' component='input' type='text' placeholder='Email' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Password</ControlLabel>
-      <Field name='password' component='input' type='password' placeholder='Password' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <div className='checkbox'>
-        <label>
-          <Field name='rememberMe' component='input' type='checkbox' id='rememberMe' label='remember me' /> remember me
-        </label>
-      </div>
-    </FormGroup>
-    <FormGroup>
-      <Button type='submit' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Sign in</Button>
-    </FormGroup>
-  </Form>
+  <div>
+    <Form onSubmit={props.handleSubmit(props.login)} form='form'>
+      <FormGroup>
+        <ControlLabel>Email</ControlLabel>
+        <Field name='email' component='input' type='text' placeholder='Email' className='form-control' />
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>Password</ControlLabel>
+        <Field name='password' component='input' type='password' placeholder='Password' className='form-control' />
+      </FormGroup>
+      <FormGroup>
+        <div className='checkbox'>
+          <label>
+            <Field name='rememberMe' component='input' type='checkbox' id='rememberMe' label='remember me' /> remember me
+          </label>
+        </div>
+      </FormGroup>
+      <FormGroup>
+        <Button type='submit' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Sign in</Button>
+      </FormGroup>
+    </Form>
+    <Button bsStyle='link' onClick={() => browserHistory.push('/register')}>register</Button>
+  </div>
 )
 
 export default reduxForm({
